6ab6b4ffe380cbcb9596a40a0c53cac6dfd81b75
benkivuva <benkivuvak5@gmail.com>
2023-05-09 05:52:39 -0700

    linked lists task

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -2,19 +2,18 @@
 #define LISTS_H
 #include <stdlib.h>
 #include <sys/types.h>
-
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- *
+ * for ALX  project
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
@@ -23,4 +22,4 @@ void free_listint(listint_t *head);
 
 listint_t *insert_node(listint_t **head, int number);
 
-#endif /* LISTS_H *
+#endif /* LISTS_H */
