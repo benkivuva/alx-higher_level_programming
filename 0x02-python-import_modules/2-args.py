c52037927c4dacc769cbab83cbc965f0128af758
benkivuva <benkivuvak5@gmail.com>
2023-05-11 07:00:02 -0700

    correction

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
-from sys import argv
 if __name__ == "__main__":
-    fname, numbers = argv[0], argv[1:]
-    argc = len(argv) - 1
-    if argc == 0:
-        print(argc, "arguments.")
-    else:
-        if argc < 2:
-            print(argc, "argument:")
-        else:
-            print(argc, "arguments:")
-        for i in range(argc):
-            print("{}:".format(i + 1), argv[i + 1])
+    import sys
+    sum = 0
+    for i in range(len(sys.argv) - 1):
+        sum += int(sys.argv[i + 1])
+    print(sum)
