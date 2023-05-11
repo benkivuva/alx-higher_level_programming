99019e21dae73610523b22390a291bf2cdd00177
benkivuva <benkivuvak5@gmail.com>
2023-05-11 07:26:19 -0700

    handle errors

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,17 +3,14 @@ if __name__ == "__main__":
     import sys
 
     total_sum = 0
+    arguments = sys.argv[1:]  # Exclude the script name
 
-    num_arguments = len(sys.argv) - 1
-    argument_string = "argument" if num_arguments == 1 else "arguments"
-    print(f"Number of {argument_string}: {num_arguments}")
-
-    for index, value in enumerate(sys.argv[1:], start=1):
-        print(f"{index}: {value}")
-        try:
-            argument = int(value)
-            total_sum += argument
-        except ValueError:
-            pass
-
-    print("Total sum:", total_sum)
+    if len(arguments) == 0:
+        print("0 arguments.")
+    elif len(arguments) == 1:
+        print("1 argument:")
+        print("1:", arguments[0])
+    else:
+        print(f"{len(arguments)} arguments:")
+        for index, argument in enumerate(arguments, start=1):
+            print(f"{index}: {argument}")
