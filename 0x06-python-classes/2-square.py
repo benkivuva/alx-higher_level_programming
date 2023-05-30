31487720be34f98661e97982f45b7f287fb5ca45
benkivuva <benkivuvak5@gmail.com>
2023-05-30 05:09:15 -0700

    update

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+class Square:
+    """Defines a square."""
+
+    def __init__(self, size=0):
+        """Constructor.
+
+        Args:
+            size (int): Length of a side of the square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
