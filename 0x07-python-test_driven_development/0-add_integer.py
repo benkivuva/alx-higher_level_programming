4a14f19377d26da67a106e69f58e3ca239a282da
Benkivuva <benkivuvak5@gmail.com>
2023-06-05 15:19:10 +0300

    initial

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+The module is add_interger
+It adds two intergers together
+"""
+
+def add_integer(a, b=98):
+    """
+    Add two integers.
+
+    Parameters:
+    a (int or float): The first integer or float.
+    b (int or float): The second integer or float. Default is 98.
+
+    Returns:
+    int: The sum of a and b as an integer.
+
+    Raises:
+    TypeError: If a or b is not an integer or float.
+    """
+
+    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+        raise TypeError("a must be an integer or b must be an integer")
+
+    a = int(a)
+    b = int(b)
+
+    return a + b
