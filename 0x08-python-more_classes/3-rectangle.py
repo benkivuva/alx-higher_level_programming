a862d453bbbe2e32edb70fbf786aaae672ee0583
Benkivuva <benkivuvak5@gmail.com>
2023-06-05 17:00:28 +0300

    update

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+
+"""This module defines Rectangle class"""
+
+
+class Rectangle:
+    """Rectangle class"""
+
+    def __init__(self, width=0, height=0):
+        """constructor"""
+        self.width, self.height = width, height
+
+    @property
+    def width(self):
+        """width getter"""
+        return self.__width
+
+    @property
+    def height(self):
+        """height getter"""
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """set width"""
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """set height"""
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
+
+    def area(self):
+        """return area of the rectangle"""
+        return (self.width * self.height)
+
+    def perimeter(self):
+        """return perimeter of the rectangle"""
+        if self.width * self.height == 0:
+            return (0)
+        return ((self.width + self.height) * 2)
+
+    def to_string(self):
+        """convert instance to string"""
+        if self.width * self.height == 0:
+            return ("")
+        return ("\n".join(["#" * self.width for i in range(self.height)]))
+
+    def __str__(self):
+        """string representation of instance"""
+        return self.to_string()
