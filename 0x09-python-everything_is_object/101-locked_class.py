01e13ce74ff6e08b895e811739aa0d8002d9863a
benkivuva <benkivuvak5@gmail.com>
2023-06-06 08:41:56 -0700

    advanced

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+class LockedClass:
+    __slots__ = ['first_name']
+
+    def __setattr__(self, name, value):
+        if name != 'first_name':
+            raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{name}'")
+        self.__dict__[name] = value
