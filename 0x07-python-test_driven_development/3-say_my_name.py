f1f075474fd4390cf75b932b882c653432f5becb
Benkivuva <benkivuvak5@gmail.com>
2023-06-06 12:19:03 +0300

    say my name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def say_my_name(first_name, last_name=""):
+    """Prints the name in formart "My name is <first_name> <last_name>".
+
+    Args:
+        first_name (str): The first name
+        last_name (str): The last name. Defaults to an empty string.
+
+
+    Raises:
+        TypeError: If first_name or last_name is not a string.
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("First name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("Last name must be a string")
+
+    full_name = " ".join(filter(None, [first_name, last_name]))
+    print("My name is {}".format(full_name))
