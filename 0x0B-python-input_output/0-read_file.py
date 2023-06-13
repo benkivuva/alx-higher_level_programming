c767367b919ee1e5e61dcdfb996b751bcffb1746
Benkivuva <benkivuvak5@gmail.com>
2023-06-13 17:22:19 +0300

    task 0

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Define read_file function"""
+
+
+def read_file(filename=""):
+    """reads a text file (UTF8) and prints it to stdout"""
+    with open(filename, "r", encoding="utf-8") as file:
+        for line in file:
+            print(line, end="")
