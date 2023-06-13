b447ff4fbf9aa31ebcaf05a87613d2442d1ddd79
Benkivuva <benkivuvak5@gmail.com>
2023-06-13 17:35:26 +0300

    modified

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,8 +4,6 @@
 
 def number_of_lines(filename=""):
     """ returns the number of lines of a text file """
-    line_count = 0
-    with open(filename, "r") as file:
-        for line in file:
-            line_count += 1
-    return line_count
+ with open(filename, encoding='utf-8') as fil:
+        return len(fil.readlines())
+
