b51659d1a718e37e044eeabfe13e0cde5152b35a
Benkivuva <benkivuvak5@gmail.com>
2023-06-13 18:34:30 +0300

    task 4

--- 0x0B-python-input_output/4-append_write.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-""" define write_file function"""
-
-
-def append_write(filename="", text=""):
-    """ write text to filename """
-    with open(filename, 'a', encoding='utf-8') as f:
-        return (f.write(text))
