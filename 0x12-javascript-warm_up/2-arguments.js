902153d2c721bcc4d04168bb60d1f1753b78843e
Benkivuva <benkivuvak5@gmail.com>
2023-08-14 18:09:40 +0300

    update

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,13 @@
 #!/usr/bin/node
-switch (process.argv.length) {
-    case 2:
-      console.log('No argument');
-      break;
-    case 3:
-      console.log('Argument found');
-      break;
-    default:
-      console.log('Arguments found');
-  }
-  
+// script that prints a message depending of the number of arguments passed
+// If no arguments are passed to the script, print “No argument”
+
+const arg = process.argv.slice(2);
+
+if (arg.length === 0) {
+  console.log('No argument');
+} else if (arg.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
