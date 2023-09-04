d93f5406103a18f344c4c0272550b324bef59d38
Benson Kivuva <122920308+benkivuva@users.noreply.github.com>
2023-09-04 09:42:11 +0300

    Update 4-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
 
     import requests
 
-    response = requests.get('https://intranet.hbtn.io/status')
+    response = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:\n\t- type: {}\n\t- content: {}".format(
         response.text.__class__, response.text))
